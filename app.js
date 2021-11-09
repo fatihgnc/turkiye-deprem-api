58962fc6fa3d421dd428c1cc23318b54bf37b92e
Fatih Genç <gnc.fath@gmail.com>
2021-11-10 00:30:53 +0300

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 class Earthquake {
     constructor(tarih, enlem, boylam, derinlik, buyukluk, lokasyon) {
         this.tarih = tarih
-        this.enlem = enlem 
+        this.enlem = enlem
         this.boylam = boylam
-        this.derinlik = derinlik 
-        this.buyukluk = buyukluk 
+        this.derinlik = derinlik
+        this.buyukluk = buyukluk
         this.lokasyon = lokasyon
     }
 }
@@ -14,6 +14,7 @@ class Earthquake {
     const express = require('express')
     const axios = require('axios')
     const cheerio = require('cheerio')
+    const cors = require('cors')
 
     const app = express()
 
@@ -37,33 +38,35 @@ class Earthquake {
     let earthquakeData = []
 
     for (let i = 0; i < rawData.length; i++) {
-        formattedData.push(rawData[i].split(/\s{1,}/g))     
+        formattedData.push(rawData[i].split(/\s{1,}/g))
     }
 
     // removing unwanted data
-    for(let i = 0; i < formattedData.length; i++) {
+    for (let i = 0; i < formattedData.length; i++) {
         let data = formattedData[i]
 
         data.splice(5, 1)
         data.splice(6, 1)
         data.splice(data.length - 1, 1)
 
-        if (data[data.length - 1].match(/\d{4}\.\d{2}\.\d{2}/)) 
+        if (data[data.length - 1].match(/\d{4}\.\d{2}\.\d{2}/))
             data.splice(data.length - 2, 2)
     }
 
     // storing data as an object in array
-    for(let i = 0; i < formattedData.length; i += 1) {
+    for (let i = 0; i < formattedData.length; i += 1) {
         let data = formattedData[i]
-        
+
         // destructuring info from the formatted earthquake data
-        let [tarih, enlem, boylam, derinlik, buyukluk, lokasyon] = [data[0] + ' ' + data[1], data[2], data[3], data[4], data[5], data.slice(6, )]
+        let [tarih, enlem, boylam, derinlik, buyukluk, lokasyon] = [data[0] + ' ' + data[1], data[2], data[3], data[4], data[5], data.slice(6,)]
         lokasyon = lokasyon.join('-')
 
         let earthquake = new Earthquake(tarih, enlem, boylam, derinlik, buyukluk, lokasyon)
 
         earthquakeData.push(earthquake)
     }
+
+    app.use(cors())
 
     // any request coming to this route will be redirected to /api route
     app.get('/', (req, res) => {
@@ -77,56 +80,56 @@ class Earthquake {
 
     // route for filtering data
     app.get('/api/filter', (req, res) => {
-        
+
         // checking if user queries in an invalid way
         let queries = Object.keys(req.query)
         const allowedFilters = ['lokasyon', 'min_buyukluk', 'max_buyukluk', 'tarih_o', 'tarih_s']
         const isValidOperation = queries.every(query => allowedFilters.includes(query))
 
-        if(!isValidOperation) {
+        if (!isValidOperation) {
             return res.status(400).send({
                 error: 'geçersiz filtre girdiniz!'
             })
         }
         // users have to provide at least one filter on this route so we have to check that
-        if(Object.keys(req.query).length === 0) {
+        if (Object.keys(req.query).length === 0) {
             return res.status(400).send({
                 error: 'en az 1 filtre girmelisiniz!'
             })
         }
-        
+
         // duplicating the earthquake data into this array so we can filter it as according to query object and send it via response
         let filteredData = []
 
-        for(let i = 0; i < earthquakeData.length; i++) {
+        for (let i = 0; i < earthquakeData.length; i++) {
             filteredData.push(earthquakeData[i])
         }
-        
+
         // filtering data
-        if(req.query.tarih_o) {
+        if (req.query.tarih_o) {
             filteredData = filteredData.filter(data => Date.parse(req.query.tarih_o) >= Date.parse(data.tarih))
         }
 
-        if(req.query.tarih_s) {
+        if (req.query.tarih_s) {
             filteredData = filteredData.filter(data => Date.parse(req.query.tarih_s) <= Date.parse(data.tarih))
         }
 
-        if(req.query.min_buyukluk) {
+        if (req.query.min_buyukluk) {
             filteredData = filteredData.filter(data => parseFloat(req.query.min_buyukluk) <= parseFloat(data.buyukluk))
         }
 
-        if(req.query.max_buyukluk) {
+        if (req.query.max_buyukluk) {
             filteredData = filteredData.filter(data => parseFloat(req.query.max_buyukluk) >= parseFloat(data.buyukluk))
         }
 
-        if(req.query.lokasyon) {
+        if (req.query.lokasyon) {
             filteredData = filteredData.filter(data => data.lokasyon.includes(req.query.lokasyon.toUpperCase()))
         }
 
         // allowing access
         res.setHeader('Access-Control-Allow-Origin', '*')
         // if no data matches the filters, then return bad request
-        if(filteredData.length === 0) {
+        if (filteredData.length === 0) {
             return res.status(400).send({
                 error: 'Girdiğiniz filtrelere uygun bir sonuç bulunamadı.'
             })
